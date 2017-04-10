1ddce70e33fa3618af2263e86022257462c8032a
v.krivovyazi <v.krivovyazi@VKRIVOVYAZI-DEL>
2017-04-10 17:56:39 +0300

    Add code from Ch7

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,13 +1,9 @@
-﻿using Moq;
-using Ninject;
+﻿using Ninject;
 using SportsStore.Domain.Abstract;
-using SportsStore.Domain.Entities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SportsStore.Domain.Concrete;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -33,15 +29,7 @@ namespace SportsStore.WebUI.Infrastructure
         }
         private void AddBindings()
         {
-            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product {Name = "Footbal", Price = 25 },
-                new Product {Name = "Surf board", Price = 179 },
-                new Product {Name= "Running shoes", Price = 95 }
-            }.AsQueryable());
-
-            ninjectKernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+            ninjectKernel.Bind<IProductsRepository>().To<EFProductRepository>();
         }
     }
 }
